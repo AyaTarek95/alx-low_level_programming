83b950431b62c1f9a8dfe756e5544b0fc8792df3
AyaTarek95 <aya.tarek213@icloud.com>
2023-06-15 13:20:41 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
@@ -8,7 +7,7 @@
 *
 *Return: Always 0 (success)
 */
-int main(void);
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
