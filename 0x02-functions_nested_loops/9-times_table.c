a5cf0345773c6da86e1325a82d28e484e6e5d700
AyaTarek95 <aya.tarek213@icloud.com>
2023-06-20 15:47:10 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * times_table - Short description, single line
+ *
+ *
+ * Return: Description of the returned value
+ */
+void times_table(void)
+{
+	int num, multi, prod;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar(48);
+		for (multi = 1; multi <= 9; multi++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = num * multi;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + 48);
+			_putchar((prod % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
