8d409d25c1f148857937a655db162950a3fecdd5
AyaTarek95 <aya.tarek213@icloud.com>
2023-06-20 16:58:59 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,12 +8,16 @@
  */
 void print_to_98(int n)
 {
-	while (n > 98)
+	for (n > 98)
+	{
 		n--;
 			printf("%d, ", n);
+	}
 	else
-		n < 98;
-	n++;
+		for (n < 98;)
+		{
+			n++;
 	printf("%d, ", n);
+		}
 	printf("98\n");
 }
