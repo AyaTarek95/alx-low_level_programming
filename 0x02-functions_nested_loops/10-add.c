38551abc6328fdd66bb8e97b00d1940612685dfc
AyaTarek95 <aya.tarek213@icloud.com>
2023-06-20 15:56:15 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - Short description, single line
+ * @a: Description of parameter x
+ * @b: Description of parameter x
+ *
+ * Return: Description of the returned value
+ */
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
