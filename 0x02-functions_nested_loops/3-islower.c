5689c2780800f6b58abb0f514519003094956a43
AyaTarek95 <aya.tarek213@icloud.com>
2023-06-20 14:25:29 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _islower - Short description, single line
+* @c: Description of parameter x
+*
+* Return: Description of the returned value
+*/
+int _islower(int c)
+{
+	int c;
+
+	for (c = 97 && c <= 122)
+		return (1);
+	return (0);
+}
+
