d9710e77767ca6ff572fc509ea4e7d58dff69e1a
AyaTarek95 <aya.tarek213@icloud.com>
2023-06-20 13:35:11 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,15 +6,14 @@
 
 void print_alphabet_x10(void)
 {
-	int line;
-	char ch;
+	int line, ch;
 
 	for (line = 0, line <= 9, line++)
 	{
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		_putchar(ch);
-	_putchar('\n');
 	}
+	_putchar('\n');
 	}
 }
