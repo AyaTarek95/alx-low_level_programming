138bc95e93a03ac8a94a64e55782864f88ff8e2e
AyaTarek95 <aya.tarek213@icloud.com>
2023-06-20 10:04:27 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,9 +9,9 @@ int main(void)
 	char str[] = "_putchar";
 	int ch;
 
-	for (ch = 0, ch < 8, ch++)
-		_putchar(str[ch]);
-		_putchar('\n');
+	for (ch = 0, ch < 8, ch++;)
+		putchar(str[ch]);
+		putchar('\n');
 
 		return (0);
 	}
