2bd661aa5add3951914489253991ab02fcf6ad62
AyaTarek95 <aya.tarek213@icloud.com>
2023-06-20 11:01:22 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*main - entry point
+*Description: print_alphabet using _putchar function to print
+*						the alphabet a_z
+*return : 0 (success)
+*/
+
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar('\n');
+}
