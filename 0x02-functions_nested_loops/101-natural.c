7545a05677a9a94e8a2a38384ef161818afe101e
AyaTarek95 <aya.tarek213@icloud.com>
2023-06-21 16:04:07 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Short description, single line
+ *
+ * Return: Description of the returned value
+ */
+int main(void)
+{
+	int sum, num;
+
+	for (num = 0; num < 1024; num++)
+	{
+		if ((num % 3 == 0) || (num % 5 == 0)
+				sum += num;
+			}
+			printf("%d\n", sum);
+			return (0);
+}
