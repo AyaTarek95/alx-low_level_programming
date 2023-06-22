e6d2fccbb0dd17e26dc1953dfff92ecbf9eb961a
AyaTarek95 <aya.tarek213@icloud.com>
2023-06-22 15:57:01 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+/**
+ * (function_name) - Short description, single line
+ * @(Variable): Description of parameter x
+ *
+ * Return: Description of the returned value
+ */
+void print_numbers(void)
+{
+	int n = 0;
+	if (n <= 0 || n <= 9; n++)
+		_putchar(n + 48);
+	_putchar(\n);
+}
