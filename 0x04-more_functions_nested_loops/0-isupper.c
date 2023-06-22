4753d349a839eba4da8be13b93b831b46078fcec
AyaTarek95 <aya.tarek213@icloud.com>
2023-06-22 02:03:34 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+include "main.h"
+/**
+ * _isupper - Short description, single line
+ * @c: Description of parameter x
+ *
+ * Return: Description of the returned value
+ */
+int _isupper(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
