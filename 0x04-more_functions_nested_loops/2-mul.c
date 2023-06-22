c8518211a5987d077779fe5e3b2c5cba699410af
AyaTarek95 <aya.tarek213@icloud.com>
2023-06-22 15:08:37 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,10 @@
+/**
+ * mul - Short description, single line
+ * @a: Description of parameter x
+ * @b: Description of parameter x
+ * Return: Description of the returned value
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
