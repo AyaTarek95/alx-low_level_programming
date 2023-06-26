a8e658ff51877a1a37dda0d553058b5504e1dd30
AyaTarek95 <aya.tarek213@icloud.com>
2023-06-26 11:52:14 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_rev - Short description, single line
+ * @s: Description of parameter x
+ *
+ * Return: Description of the returned value
+ */
+void print_rev(char *s)
+{
+	int len = strlen(s);
+
+	for (int i = len - 1; i >= 0; i--)
+	{
+		printf("%c", s[i]);
+	}
+	printf("\n");
+}
