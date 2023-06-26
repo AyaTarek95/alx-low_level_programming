8c4f5e534dd9e093c42ce1a493e0398619f57853
AyaTarek95 <aya.tarek213@icloud.com>
2023-06-26 11:13:00 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - Short description, single line
+ * @str: Description of parameter x
+ *
+ * Return: Description of the returned value
+ */
+void _puts(char *str)
+{
+	while (*str != '\0';
+	{
+		_putchar(*str + 0);
+		str++;
+		}
+	_putchar(\n);
+	}
