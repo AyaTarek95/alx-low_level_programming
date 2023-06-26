1dd3bcbdb223ed2c4ad90f9013ba1471058848d7
AyaTarek95 <aya.tarek213@icloud.com>
2023-06-26 13:10:16 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - Short description, single line
+ * @str: Description of parameter x
+ *
+ * Return: Description of the returned value
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
