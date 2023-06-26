63175b0dfc2d80e2a8c4105e201cc8472e492924
AyaTarek95 <aya.tarek213@icloud.com>
2023-06-26 12:53:11 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,10 +1,21 @@
 #include "main.h"
 /**
- * (function_name) - Short description, single line
- * @(Variable): Description of parameter x
+ * rev_string - Short description, single line
+ * @s: Description of parameter x
  *
  * Return: Description of the returned value
  */
 void rev_string(char *s)
 {
+	int i, c, l;
+	char h;
 
+	for (i = 0; s[i] != '\0'; i++)
+		l = i;
+	for (i--, c = 0; c < l / 2; i--, c++)
+	{
+		h = s[c];
+		s[c] = s[i];
+		s[i] = h;
+	}
+}
