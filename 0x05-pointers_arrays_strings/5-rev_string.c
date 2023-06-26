c7cde4c98843167e7f4d59574b2254376524b522
AyaTarek95 <aya.tarek213@icloud.com>
2023-06-26 12:26:10 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * (function_name) - Short description, single line
+ * @(Variable): Description of parameter x
+ *
+ * Return: Description of the returned value
+ */
+void rev_string(char *s)
+{
+
