9c09cd209ccb555346587dc13375f96a6730f840
AyaTarek95 <aya.tarek213@icloud.com>
2023-06-26 10:52:30 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*reset_to_98 - Entry point
+*
+*@n : A C program that prints with put function
+*
+*Return: Always 0 (success)
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
