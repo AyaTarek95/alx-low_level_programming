0038fd14f12db71812aec267b4fb002b7a403e5b
AyaTarek95 <aya.tarek213@icloud.com>
2023-06-26 12:01:24 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,12 +7,11 @@
  */
 void print_rev(char *s)
 {
-	int len = strlen(s);
-	int i = 0;
+	int i;
 
-	for (int i = len - 1; i >= 0; i--)
-	{
-		printf("%c", s[i]);
-	}
-	printf("\n");
+	for (i = 0; s[i] != '\0'; i++ )
+	for (i--; i>= 0; i--)
+		_putchar(s[i]);
+	
+	_putchar('\n');
 }
