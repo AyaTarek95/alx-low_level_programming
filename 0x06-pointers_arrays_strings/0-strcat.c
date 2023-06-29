4ccea84d94ad1968034b01c01d284371e05fcc84
AyaTarek95 <aya.tarek213@icloud.com>
2023-06-29 05:49:16 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strcat  - Short description, single line
+ * @src: Description of parameter x
+ * @dest : desc
+ * Return: return dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int c, c2;
+
+	c = 0;
+	while (dest[c])
+		c++;
+	for (c2 = 0; src[c2]; c2++)
+		dest[c++] = src[c2];
+	return (dest);
+}
