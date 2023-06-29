fa93965eb93a66803712172bf45b2180cda14091
AyaTarek95 <aya.tarek213@icloud.com>
2023-06-29 08:02:08 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*_strcmp - Entry point
+*
+*@s1: A C program that prints with put function
+*@s2: 2nd
+*Return: Always 0 (success)
+*/
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
