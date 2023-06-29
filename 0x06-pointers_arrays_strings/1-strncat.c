24bcb3e908fbb00690988a94c0df772f971cb307
AyaTarek95 <aya.tarek213@icloud.com>
2023-06-29 07:41:55 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,11 @@
+/**
+ * _strncat  - Short description, single line
+ * @dest: desc
+ * @src: source
+ * Return: return dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	return (strncat(dest, src, n));
+}
