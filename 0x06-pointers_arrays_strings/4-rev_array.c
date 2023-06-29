dab9d143372d300f22eca331e5f817e7af013933
AyaTarek95 <aya.tarek213@icloud.com>
2023-06-29 09:10:25 -0700

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*reverse_array - Entry point
+*
+*@a: A C program that prints with put function
+*@n:
+*Return: Always 0 (success)
+*/
+void reverse_array(int *a, int n)
+{
+	int i, tmp;
+	for (i =0; i < n/2; i++)
+	{
+	tmp = [n - 1];
+	a[i] = [n - i - 1];
+	 a[i] = tmp;
+	}
+}
