f31027b8438d74186e0041304bb3bbe2cc8e0dc1
AyaTarek95 <aya.tarek213@icloud.com>
2023-06-29 07:57:49 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
-* strncpy- Entry point
+* _strncpy- Entry point
 *
 *@dest: A C program that prints with put function
 *@src: 2nd
-@n: 3rd
+*@n: 3rd
 *Return: Always 0 (success)
 */
 char *_strncpy(char *dest, char *src, int n)
 {
-	return (strncpy(dest, src, n);
+	return (strncpy(dest, src, n));
 }
