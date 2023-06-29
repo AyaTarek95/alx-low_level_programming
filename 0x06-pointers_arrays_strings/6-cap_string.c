45a6bab1464efb8ef75f281289ae7d52eab4411f
AyaTarek95 <aya.tarek213@icloud.com>
2023-06-29 12:42:27 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,11 +1,43 @@
 #include "main.h"
 /**
- * cap_string - Short description, single line
- * @str: Description of parameter x
+ * isUpperLetter - Checks whether char, cap or not
+ * @c: Char to check
  *
- * Return: Description of the returned value
+ * Return: 1 if capital, -1 if small, 0 otherwise
  */
-char *cap_string(char *str)
-{
-	
+
+	int isUpperLetter(char c)
+	{
+	int i = 0;
+
+	if (c > 64 && c < 91)
+	i++;
+	else if (c == ',' || c == ';' || c == '.' || c == '!' || c == '?' ||
+	c == '"' || c == '(' || c == ')' || c == '{' ||
+	c == '}' || c == ' ' || c == 9 || c == '\n')
+	i = -2;
+		else if (c > 96 && c < 123)
+		i--;
+
+	return (i);
+}
+
+/**
+ * cap_string - Capitalizes letters
+ * @c: ptr to array
+ *
+ * Return: ptr to array
+ */
+
+		char *cap_string(char *c)
+		{
+		int i, checker = -2;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+	if (checker == -2 && isUpperLetter(c[i]) == -1)
+	c[i] -= 32;
+	checker = isUpperLetter(c[i]);
+	}
+return (c);
 }
