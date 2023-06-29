163397604ef0fbe3764c18438e598c8e8a3c4bde
AyaTarek95 <aya.tarek213@icloud.com>
2023-06-29 11:09:00 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,16 +1,20 @@
 #include "main.h"
 /**
- * (function_name) - Short description, single line
- * @(Variable): Description of parameter x
+ * cap_string - Short description, single line
+ * @str: Description of parameter x
  *
  * Return: Description of the returned value
  */
-char *cap_string(char *)
+char *cap_string(char *str)
 {
-	
+	int i;
 
-
-
-
-
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] > 96 && str[i] < 123)
+		{
+		str[i] -= 32;
+		}
+	}
+	return (str);
 }
