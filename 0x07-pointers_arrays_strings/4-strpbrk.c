66cab4d9e0a32dd7e10ba3fe67405b2164ff7175
AyaTarek95 <aya.tarek213@icloud.com>
2023-07-03 13:08:23 -0700

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strpbrk - Short description, single line
+ * @s: Description of parameter x
+ * @accept: 2nd
+ *
+ * Return: Description of the returned value
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	return (strpbrk(s, accept));
+}
