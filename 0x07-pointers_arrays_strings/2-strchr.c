8f7a934ed391be2ec7805b27d1f281c3a20e4483
AyaTarek95 <aya.tarek213@icloud.com>
2023-07-03 13:00:32 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strchr - Short description, single line
+ * @s: Description of parameter x
+ * @c: 2nd
+ * Return: Description of the returned value
+ */
+char *_strchr(char *s, char c)
+{
+	return (strchr(s, c));
+}
