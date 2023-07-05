8c3f2400fef6b01f41f06a6f3e7d0dbe3135b2a3
AyaTarek95 <aya.tarek213@icloud.com>
2023-07-05 16:33:37 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -31,20 +31,3 @@ return (i);
 else
 return (_sqrt(x, i + 1));
 }
-
-/**
- * hai - helper function, recursive steps taken
- * @n: number given to original function is_prime_number
- * @d: incrementer divisor
- * Return: 0 if not prime, 1 if prime
- */
-
-int hai(int n, int d)
-{
-	if (n % d == 0)
-	return (0);
-	else if (_sqrt(n, 1) < d)
-	return (1);
-	else
-	return (hai(n, d + 1));
-}
