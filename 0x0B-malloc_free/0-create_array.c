0c76b7c4c939aeefe5ef854acc8fae498556a1f7
AyaTarek95 <aya.tarek213@icloud.com>
2023-07-11 12:42:08 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * create_array - description
+ * @size: 1st
+ * @c: 2nd
+ * Return: always 0
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *a;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	a = malloc(size * sizeof(*a));
+	if (a == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		a[i] = c;
+
+	return (a);
+}
