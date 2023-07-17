8dfae2d097ecafbf752c5590568bdf6f38206cc1
AyaTarek95 <aya.tarek213@icloud.com>
2023-07-17 06:33:54 -0700

    2nd

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - descripe struct
+ * @d: 1st
+ * @name: 2nd
+ * @age: 3rd
+ * @owner: 4th
+ * Return: always 0
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
