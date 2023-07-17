e1ec2ddf939f78d3bc4a653d1222fa6a6d80b5e4
AyaTarek95 <aya.tarek213@icloud.com>
2023-07-17 06:47:43 -0700

    3rd

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include "stdio.h"
+/**
+ * print_dog - description
+ * @d:1st
+ * Return: always 0
+ */
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		printf("%s\n", d->name ? d->name : "(nil)");
+		printf("%f\n", d->age);
+		printf("%s\n", a->owner ? d->owner : "(nil)");
+	}
+}
