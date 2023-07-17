0841614138fe72450256c0affa523d948bcc6302
AyaTarek95 <aya.tarek213@icloud.com>
2023-07-17 06:21:46 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+
+/**
+ * struct dog - a struct
+ * @name: 1st
+ * @age: 2nd
+ * @owner: 3rd
+ *
+ * Description: a dog struct
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
