a9e082aa99c05cc32e9b35fa4eca68b317d4950a
AyaTarek95 <aya.tarek213@icloud.com>
2023-07-27 13:16:29 -0700

    update 2 add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,28 +3,28 @@
 #include <string.h>
 
 /**
- * add_node - add new node at the start of linked list
- * @head: double pointer to list_t
+ * add_node - add new node at start of list
+ * @head: db pointer to list_t
  * @str: new str add in node
- * Return:  address of  new element
+ * Return: address of  new element
  */
 
 list_t *add_node(list_t **head, const char *str)
 {
 list_t *new;
-unsigned int length = 0;
+unsigned int len = 0;
 
-	while (str[length])
-		length++;
+	while (str[len])
+		len++;
 
 	new = malloc(sizeof(list_t));
 	if (!new)
 		return (NULL);
 
 new->str = strdup(str);
-new->length = length;
+new->len = len;
 new->next = (*head);
 (*head) = new;
 
-	return (*head);
+return (*head);
 }
