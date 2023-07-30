4a339f7011a602b00d2217e5cb15610ba4daa463
AyaTarek95 <aya.tarek213@icloud.com>
2023-07-30 03:08:10 -0700

    update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -23,6 +23,7 @@ dog_t *new_dog(char *name, float age, char *owner);
 int _strlen(char *s);
 char *_strcpy(char *dest, char *src);
 void free_dog(dog_t *d);
-
+char *_strcpy(char *dest, char *scr);
+int _strlen(char *s);
 
 #endif
