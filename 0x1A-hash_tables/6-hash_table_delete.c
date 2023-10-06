e96c9775eca8c21ad09303bbd5f7982974912bef
AyaTarek95 <aya.tarek213@icloud.com>
2023-10-06 01:35:10 -0700

    task6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table
+ * Return: void
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+hash_node_t *node, *temp;
+hash_table_t *head = ht;
+unsigned long int x;
+
+	for (x = 0; x < ht->size; x++)
+	{
+		if (ht->array[x] != NULL)
+		{
+			node = ht->array[x];
+			while (node != NULL)
+			{
+				temp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = temp;
+			}
+		}
+	}
+free(head->array);
+free(head);
+}
