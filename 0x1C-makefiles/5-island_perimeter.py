8b41d50dd4c1da09d81f06b8fe3445413632d3d1
AyaTarek95 <aya.tarek213@icloud.com>
2023-11-22 11:22:37 -0800

    task5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+island_perimeter
+"""
+
+def island_perimeter(grid):
+    """return perimeter of  island in grid
+
+    """
+    count = 0
+
+    for row in grid:
+        for column in row:
+            if column == 1:
+                count+= 1
+    p = (count * 2) + 2
+    return p
